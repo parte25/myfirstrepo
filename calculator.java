7efc571278e9a0148a5c957a743158a1f4a29296
parte25 <79994292+parte25@users.noreply.github.com>
2021-08-17 14:55:08 +0200

    added code for add, sub

---
+++ calculator.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class calculator
+{
+  public int add(int num1,int num2)
+  {
+    int result = num1 + num2;
+    return result;
+  }
+  public int sub(int num1,int num2)
+  {
+    int result = num1 - num2;
+    return result;
+  }
+}
